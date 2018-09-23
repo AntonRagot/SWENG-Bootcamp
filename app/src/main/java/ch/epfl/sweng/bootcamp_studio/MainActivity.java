a8bd997eb732d38eb6ad9e431d6242d18ae2620e
Anton Ragot <anton.ragot@epfl.ch>
2018-09-23 21:05:32 +0200

    Switching branch

--- app/src/main/java/ch/epfl/sweng/bootcamp_studio/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/bootcamp_studio/MainActivity.java
@@ -1,13 +1,28 @@
 package ch.epfl.sweng.bootcamp_studio;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "ch.epfl.sweng.bootcamp_studio.MESSAGE";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /* Will be called when button pressed */
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
