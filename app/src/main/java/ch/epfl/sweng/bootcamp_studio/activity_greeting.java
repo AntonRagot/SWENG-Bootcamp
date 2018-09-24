eb4a85a89e41b68541f1a05b5ae5168e2670e902
Anton Ragot <anton.ragot@epfl.ch>
2018-09-24 15:31:47 +0200

    End of bootcamp

--- app/src/main/java/ch/epfl/sweng/bootcamp_studio/activity_greeting.java
+++ app/src/main/java/ch/epfl/sweng/bootcamp_studio/activity_greeting.java
@@ -1,7 +1,9 @@
 package ch.epfl.sweng.bootcamp_studio;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class activity_greeting extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class activity_greeting extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_greeting);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.greetingMessage);
+        textView.setText("Hello " + message + "!");
+
     }
 }
